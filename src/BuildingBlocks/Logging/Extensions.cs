4583b5eca7d24671195ceb1ee2b22a79dc82b951
meysamhadeli <meysamhadeli@gmail.com>
2022-09-10 21:31:15 +0430

    refactor logging

--- src/BuildingBlocks/Logging/Extensions.cs
+++ src/BuildingBlocks/Logging/Extensions.cs
@@ -1,47 +1,67 @@
-﻿using System.Reflection;
+﻿using System.Text;
 using BuildingBlocks.Web;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
 using Serilog;
-using Serilog.Enrichers.Span;
 using Serilog.Events;
 using Serilog.Exceptions;
 using Serilog.Sinks.Elasticsearch;
 using Serilog.Sinks.SpectreConsole;
 
-namespace BuildingBlocks.Logging;
-
-public static class Extensions
+namespace BuildingBlocks.Logging
 {
-    public static WebApplicationBuilder AddCustomSerilog(this WebApplicationBuilder builder)
+    public static class Extensions
     {
-        builder.Host.UseSerilog((context, loggerConfiguration) =>
+        public static WebApplicationBuilder AddCustomSerilog(this WebApplicationBuilder builder)
         {
-            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            var loggOptions = context.Configuration.GetSection(nameof(LogOptions)).Get<LogOptions>();
-            var appOptions = context.Configuration.GetSection(nameof(AppOptions)).Get<AppOptions>();
+            builder.Host.UseSerilog((context, services, loggerConfiguration) =>
+            {
+                var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+                var logOptions = context.Configuration.GetSection(nameof(LogOptions)).Get<LogOptions>();
+                var appOptions = context.Configuration.GetSection(nameof(AppOptions)).Get<AppOptions>();
 
-            var logLevel = Enum.TryParse<LogEventLevel>(loggOptions.Level, true, out var level)
-                ? level
-                : LogEventLevel.Information;
 
-            loggerConfiguration
-                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(loggOptions.ElasticUri))
+                var logLevel = Enum.TryParse<LogEventLevel>(logOptions.Level, true, out var level)
+                    ? level
+                    : LogEventLevel.Information;
+
+                loggerConfiguration
+                    .MinimumLevel.Is(logLevel)
+                    .WriteTo.SpectreConsole(logOptions.LogTemplate, logLevel)
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                    // Only show ef-core information in error level
+                    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Error)
+                    // Filter out ASP.NET Core infrastructure logs that are Information and below
+                    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                    .Enrich.WithExceptionDetails()
+                    .Enrich.FromLogContext()
+                    .ReadFrom.Configuration(context.Configuration);
+
+                if (logOptions.Elastic.Enable)
                 {
-                    AutoRegisterTemplate = true,
-                    IndexFormat =
-                        $"{appOptions.Name}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
-                })
-                .WriteTo.SpectreConsole(loggOptions.LogTemplate, logLevel)
-                .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Error)
-                .Enrich.WithSpan()
-                .Enrich.WithExceptionDetails()
-                .Enrich.FromLogContext()
-                .ReadFrom.Configuration(context.Configuration);
-        });
+                    loggerConfiguration.WriteTo.Elasticsearch(
+                        new ElasticsearchSinkOptions(new Uri(logOptions.Elastic.ElasticServiceUrl))
+                        {
+                            AutoRegisterTemplate = true,
+                            IndexFormat =
+                                $"{appOptions.Name}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+                        });
+                }
 
-        return builder;
+                if (logOptions.File.Enable)
+                {
+                    var path = string.IsNullOrWhiteSpace(logOptions.File.Path) ? "logs/.txt" : logOptions.File.Path;
+                    if (!Enum.TryParse<RollingInterval>(logOptions.File.Interval, true, out var interval))
+                    {
+                        interval = RollingInterval.Day;
+                    }
+
+                    loggerConfiguration.WriteTo.File(path, rollingInterval: interval, encoding: Encoding.UTF8,
+                        outputTemplate: logOptions.LogTemplate);
+                }
+            });
+
+            return builder;
+        }
     }
 }
---
+++ src/BuildingBlocks/Logging/LogOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace BuildingBlocks.Logging
+{
+    public class LogOptions
+    {
+        public string Level { get; set; }
+        public ElasticOptions Elastic { get; set; }
+        public FileOptions File { get; set; }
+        public string LogTemplate { get; set; }
+    }
+}
