c5f35ac12ac9b5649c9975f293945ce7b6d36f6f
meysamhadeli <meysamhadeli@gmail.com>
2023-01-16 01:02:20 +0330

    test: Add unit tests for flight domain models

---
+++ src/Services/Flight/tests/UnitTest/Fakes/FakeFlightUpdate.cs
@@ -0,0 +1,12 @@
+﻿namespace Unit.Test.Fakes;
+
+using global::Flight.Flights.Models;
+
+public static class FakeFlightUpdate
+{
+    public static void Generate(Flight flight)
+    {
+        flight.Update(flight.Id, flight.FlightNumber, 3, flight.DepartureAirportId, flight.DepartureDate,
+            flight.ArriveDate, 3, flight.DurationMinutes, flight.FlightDate, flight.Status, flight.Price, flight.IsDeleted);;
+    }
+}
