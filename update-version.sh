5c958636e95472023c9ab85e393a6e515c7cf4df
meysamhadeli <meysamhadeli@gmail.com>
2023-01-25 03:05:26 +0330

    .

---
+++ update-version.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/bash
+
+# update package version and version for in the csproj file to new version and commit it again with using https://github.com/semantic-release/git plugin
+# https://unix.stackexchange.com/questions/50313/how-do-i-perform-an-action-on-all-files-with-a-specific-extension-in-subfolders
+find . -name '*.Packages.props' -exec sed -i "s#<PackageVersion>.*#<PackageVersion>$1</PackageVersion>#" {} \;  -exec cat {} \;
+find . -name '*.Packages.props' -exec sed -i "s#<InformationalVersion>.*#<InformationalVersion>$1</InformationalVersion>#" {} \;  -exec cat {} \;
+find . -name '*.Packages.props' -exec sed -i "s#<Version>.*#<Version>$1</Version>#" {} \;  -exec cat {} \;
