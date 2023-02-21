20795825a38dae185dc6268580c38bab1e4b80da
Pc <meysamhadeli@gmail.com>
2023-02-21 22:19:42 +0330

    chore: Update kubernetes configs

--- src/Services/Flight/tests/IntegrationTest/Fakes/FakeCreateAircraftCommand.cs
+++ src/Services/Flight/tests/IntegrationTest/Fakes/FakeCreateAircraftCommand.cs
@@ -1,6 +1,5 @@
 ﻿using AutoBogus;
 using BuildingBlocks.IdsGenerator;
-using Flight.Aircrafts.Features.CreateAircraft;
 using Flight.Aircrafts.Features.CreateAircraft.Commands.V1;
 
 namespace Integration.Test.Fakes;
