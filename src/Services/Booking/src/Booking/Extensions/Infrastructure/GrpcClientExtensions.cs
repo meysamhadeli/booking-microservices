89657e832b99e6765cfe23d951d75f75b700fcf3
Meysam Hadeli <35596795+meysamhadeli@users.noreply.github.com>
2024-09-16 02:17:30 +0330

    feat: add husky for handling pre commit rules

--- src/Services/Booking/src/Booking/Extensions/Infrastructure/GrpcClientExtensions.cs
+++ src/Services/Booking/src/Booking/Extensions/Infrastructure/GrpcClientExtensions.cs
@@ -27,7 +27,6 @@ public static class GrpcClientExtensions
         })
             .AddGrpcRetryPolicyHandler()
             .AddGrpcCircuitBreakerPolicyHandler();
-        ;
 
         return services;
     }
