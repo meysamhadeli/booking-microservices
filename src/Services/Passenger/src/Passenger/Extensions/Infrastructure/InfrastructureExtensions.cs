9f284b3604c51cb5f9aa98a15bf109835519cc9d
Meysam Hadeli <35596795+meysamhadeli@users.noreply.github.com>
2025-02-14 01:23:38 +0330

    feat: add full observability top of open telemetry collector

--- src/Services/Passenger/src/Passenger/Extensions/Infrastructure/InfrastructureExtensions.cs
+++ src/Services/Passenger/src/Passenger/Extensions/Infrastructure/InfrastructureExtensions.cs
@@ -55,12 +55,12 @@ public static class InfrastructureExtensions
                             partitionKey: httpContext.User.Identity?.Name ??
                                           httpContext.Request.Headers.Host.ToString(),
                             factory: partition => new FixedWindowRateLimiterOptions
-                                                  {
-                                                      AutoReplenishment = true,
-                                                      PermitLimit = 10,
-                                                      QueueLimit = 0,
-                                                      Window = TimeSpan.FromMinutes(1)
-                                                  }));
+                            {
+                                AutoReplenishment = true,
+                                PermitLimit = 10,
+                                QueueLimit = 0,
+                                Window = TimeSpan.FromMinutes(1)
+                            }));
             });
 
         builder.Services.AddPersistMessageProcessor();
